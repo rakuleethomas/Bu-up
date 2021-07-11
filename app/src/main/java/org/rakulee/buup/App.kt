c9317c5e35e90f8b2c8c2926cd5256866cb6f14d
Won Kyu Jeong <56411475+wonkyjunky@users.noreply.github.com>
2021-07-11 14:09:50 -0700

    job query

--- app/src/main/java/org/rakulee/buup/App.kt
+++ app/src/main/java/org/rakulee/buup/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.parse.Parse
 import com.parse.ParseObject
 import dagger.hilt.android.HiltAndroidApp
+import org.rakulee.buup.model.Job
 import org.rakulee.buup.screens.LoginActivity
 
 @HiltAndroidApp
@@ -12,7 +13,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-
+        ParseObject.registerSubclass(Job::class.java)
         Parse.initialize(Parse.Configuration.Builder(this)
             .applicationId("aVQUhSqemZDmI8n0mWYOSPzWBAlPb4YPUCIbtH6Q") // should correspond to Application ID env variable
             .clientKey("hrcIMfNQZESdJVFY5H3g11rkvQK9d5jRWruyQjfA")  // should correspond to Client key env variable
--- app/src/main/java/org/rakulee/buup/adapters/JobListAdapter.kt
+++ app/src/main/java/org/rakulee/buup/adapters/JobListAdapter.kt
@@ -10,14 +10,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.rakulee.buup.R
 import org.rakulee.buup.databinding.ItemAdListBinding
+import org.rakulee.buup.model.Job
 import org.rakulee.buup.model.JobItem
 
 class JobListAdapter() : RecyclerView.Adapter<JobListAdapter.ViewHolder>() {
 
-    private var mItems : ArrayList<JobItem> = ArrayList<JobItem>()
+    private var mItems : ArrayList<Job> = ArrayList<Job>()
     private lateinit var context : Context
 
-    fun updateItems(items : ArrayList<JobItem>){
+    fun updateItems(items : ArrayList<Job>){
         mItems = items;
         notifyDataSetChanged()
     }
@@ -44,14 +45,14 @@ class JobListAdapter() : RecyclerView.Adapter<JobListAdapter.ViewHolder>() {
             // initialize event listeners if needed
         }
 
-        fun bind(item : JobItem){
+        fun bind(item : Job){
 //            binding.item = item;
-            var imageUrl = item.imageUrl
+            var imageUrl = item.ImageUrl
 //            val uri = Uri.parse("https://svkoreans.com/img/svlogo1-1.jpg");
             if("".equals(imageUrl)){
                 imageUrl = "https://svkoreans.com/img/svlogo1-1.jpg";
             }
-//            Glide.with(context).load(imageUrl).into(binding.ivCompanyLogo)
+            Glide.with(context).load(imageUrl).into(binding.ivCompanyLogo)
             binding.tvCompanyName.text = item.companyTitle
             binding.tvJobDescription.text = item.jobDescription
             binding.executePendingBindings()
