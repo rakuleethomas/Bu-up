35ce2d9ce0ee58c840543a3b7dd7179d518a1426
Wonkyu-Backblaze <56411475+wonkyjunky@users.noreply.github.com>
2021-08-29 19:40:38 -0700

    onboarding

--- app/src/main/java/org/rakulee/buup/fragments/jobseeker/JobSeekerOnBoarding3.kt
+++ app/src/main/java/org/rakulee/buup/fragments/jobseeker/JobSeekerOnBoarding3.kt
@@ -1,12 +1,21 @@
 package org.rakulee.buup.fragments.jobseeker
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.*
 import dagger.hilt.android.AndroidEntryPoint
 import org.rakulee.buup.R
+import org.rakulee.buup.databinding.FragmentJobSeekerOnBoarding3Binding
+import java.util.jar.Manifest
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +33,12 @@ class JobSeekerOnBoarding3 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private val permissions = arrayOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    lateinit var binding : FragmentJobSeekerOnBoarding3Binding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +53,34 @@ class JobSeekerOnBoarding3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job_seeker_on_boarding3, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_job_seeker_on_boarding3, container, false)
+        binding.lifecycleOwner = this
+        binding.onBoarding3 = this
+
+        binding.btnDone.setOnClickListener{
+            val intent : Intent
+
+        }
+
+        if (!checkPermission(permissions)){
+            requestPermissions(permissions, REQUEST_CODE_PERMISSION)
+        }
+
+
+        return binding.root
     }
 
+    private fun checkPermission(permission: Array<String>) : Boolean {
+        return permissions.all {
+            ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+
     companion object {
+
+        const val REQUEST_CODE_PERMISSION = 1001
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
