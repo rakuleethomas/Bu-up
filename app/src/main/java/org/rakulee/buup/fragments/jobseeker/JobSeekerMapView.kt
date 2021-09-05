92f1488636ac1902a562d7163becdefe5e574e76
Wonkyu-Backblaze <wjeong@csumb.edu>
2021-09-05 03:06:56 -0700

    setting up: job seeker map view/ list view

--- app/src/main/java/org/rakulee/buup/fragments/jobseeker/JobSeekerMapView.kt
+++ app/src/main/java/org/rakulee/buup/fragments/jobseeker/JobSeekerMapView.kt
@@ -1,23 +1,16 @@
 package org.rakulee.buup.fragments.jobseeker
 
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.location.*
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.rakulee.buup.R
-import org.rakulee.buup.databinding.FragmentJobSeekerOnBoarding3Binding
-import org.rakulee.buup.screens.LoginActivity
-import org.rakulee.buup.screens.PartTimeJobSeekerActivity
-import java.util.jar.Manifest
+import org.rakulee.buup.databinding.FragmentJobSeekerMapViewBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,21 +19,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [JobSeekerOnBoarding3.newInstance] factory method to
+ * Use the [JobSeekerMapView.newInstance] factory method to
  * create an instance of this fragment.
  */
-
 @AndroidEntryPoint
-class JobSeekerOnBoarding3 : Fragment() {
+class JobSeekerMapView : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private val permissions = arrayOf(
-        android.Manifest.permission.ACCESS_FINE_LOCATION,
-        android.Manifest.permission.ACCESS_COARSE_LOCATION
-    )
-    lateinit var binding : FragmentJobSeekerOnBoarding3Binding
-
+    lateinit var binding : FragmentJobSeekerMapViewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,48 +42,36 @@ class JobSeekerOnBoarding3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_job_seeker_on_boarding3, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_job_seeker_map_view, container, false)
         binding.lifecycleOwner = this
-        binding.onBoarding3 = this
+        binding.vm = this
 
-        binding.btnDone.setOnClickListener{
-            val intent = Intent(context, PartTimeJobSeekerActivity::class.java)
-            startActivity(intent)
-            activity?.finish()
+
+        binding.fab.setOnClickListener{
+            val direction : NavDirections = JobSeekerMapViewDirections.actionMainSeekerHomeToMainSeekerListview3()
+            findNavController().navigate(direction)
         }
-
-        if (!checkPermission(permissions)){
-            requestPermissions(permissions, REQUEST_CODE_PERMISSION)
-        }
-
 
 
         return binding.root
+
     }
 
-    private fun checkPermission(permission: Array<String>) : Boolean {
-        return permissions.all {
-            ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
-        }
-    }
 
 
     companion object {
-
-        const val REQUEST_CODE_PERMISSION = 1001
-
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment JobSeekerOnBoarding3.
+         * @return A new instance of fragment JobSeekerMapView.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            JobSeekerOnBoarding3().apply {
+            JobSeekerMapView().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
