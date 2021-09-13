bd844c3f1bd9d0fcf6c14be7f5cd7f45cd92bd5d
Won Kyu Jeong <56411475+wonkyjunky@users.noreply.github.com>
2021-09-13 11:48:34 -0700

    filter

--- app/src/main/java/org/rakulee/buup/fragments/jobseeker/JobSeekerFilter.kt
+++ app/src/main/java/org/rakulee/buup/fragments/jobseeker/JobSeekerFilter.kt
@@ -8,9 +8,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.common.util.DataUtils
 import dagger.hilt.android.AndroidEntryPoint
 import org.rakulee.buup.R
-import org.rakulee.buup.databinding.FragmentJobSeekerMapViewBinding
+import org.rakulee.buup.databinding.FragmentJobSeekerFilterBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,15 +20,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [JobSeekerMapView.newInstance] factory method to
+ * Use the [JobSeekerFilter.newInstance] factory method to
  * create an instance of this fragment.
  */
 @AndroidEntryPoint
-class JobSeekerMapView : Fragment() {
+class JobSeekerFilter : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    lateinit var binding : FragmentJobSeekerMapViewBinding
+    lateinit var binding : FragmentJobSeekerFilterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,27 +42,18 @@ class JobSeekerMapView : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_job_seeker_map_view, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_job_seeker_filter, container, false)
         binding.lifecycleOwner = this
         binding.vm = this
 
 
-        binding.fab.setOnClickListener{
-            val direction : NavDirections = JobSeekerMapViewDirections.actionMainSeekerHomeToMainSeekerListview3()
+        binding.btnBack.setOnClickListener{
+            val direction : NavDirections = JobSeekerFilterDirections.actionMainSeekerFilterToMainSeekerHome()
             findNavController().navigate(direction)
         }
-        binding.imageButton3.setOnClickListener{
-            val directions : NavDirections = JobSeekerMapViewDirections.actionMainSeekerHomeToMainSeekerFilter()
-            findNavController().navigate(directions);
-        }
-
 
         return binding.root
-
     }
-
-
 
     companion object {
         /**
@@ -70,12 +62,12 @@ class JobSeekerMapView : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment JobSeekerMapView.
+         * @return A new instance of fragment JobSeekerFilter.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            JobSeekerMapView().apply {
+            JobSeekerFilter().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
